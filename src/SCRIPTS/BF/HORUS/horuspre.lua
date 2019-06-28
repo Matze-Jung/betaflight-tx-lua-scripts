6d92e76eda4026bed53ee37096cc303163516de9
Matze-Jung <matthiasjung@freenet.de>
2019-06-28 21:02:32 +0200

    Add support for Jumper T12, update control schema for Horus

--- src/SCRIPTS/BF/HORUS/horuspre.lua
+++ src/SCRIPTS/BF/HORUS/horuspre.lua
@@ -16,23 +16,22 @@ MenuBox = { x=120, y=100, w=200, x_offset=68, h_line=20, h_offset=6 }
 SaveBox = { x=120, y=100, w=180, x_offset=12, h=60, h_offset=12 }
 NoTelem = {   192,   LCD_H - 28, "No Telemetry", TEXT_COLOR + INVERS + BLINK }
 
--- schema of the X9, cause currently no access to horus testing
 ctrlSchema = {
     display = {
         prevPage = {
-            cond = function(e, evs) return e == evs.longPress.page end,
+            cond = function(e, evs) return e == evs.press.pageUp end,
             func = function() stepPage(-1) end
         },
         nextPage = {
-            cond = function(e, evs) return e == evs.release.page end,
+            cond = function(e, evs) return e == evs.press.pageDown end,
             func = function() stepPage(1) end
         },
         prevLine = {
-            cond = function(e, evs) return e == evs.release.plus end,
+            cond = function(e, evs) return e == evs.dial.left end,
             func = function() stepLine(-1) end
         },
         nextLine = {
-            cond = function(e, evs) return e == evs.release.minus end,
+            cond = function(e, evs) return e == evs.dial.right end,
             func = function() stepLine(1) end
         },
         edit = {
@@ -43,16 +42,15 @@ ctrlSchema = {
             end
         },
         menu = {
-            cond = function(e, evs) return (evs.press.pageDown and e == evs.longPress.enter) or e == evs.release.menu end,
+            cond = function(e, evs) return (evs.press.pageDown and e == evs.longPress.enter) or e == 1542 end, -- 1542 => [SYS]
             func = function()
                 setState("displayMenu")
-                setLock("displayMenu.cnfrm")
+                setLock("displayMenu.exit")
             end
         },
         home = {
-            cond = function(e, evs) return e == evs.longPress.menu end,
+            cond = function(e, evs) return e == 1541 end, -- 1541 => [TELE]
             func = function()
-                setLock("display.menu")
                 gotoPage(1)
             end
         },
@@ -71,17 +69,17 @@ ctrlSchema = {
             func = function() stepValue(1) end
         },
         exit = {
-            cond = function(e, evs) return e == evs.release.enter or e == evs.release.menu or e == evs.release.exit end,
+            cond = function(e, evs) return e == evs.release.enter end,
             func = function() setState("display") end
         }
     },
     displayMenu = {
         prev = {
-          cond = function(e, evs) return e == evs.release.plus end,
+          cond = function(e, evs) return e == evs.dial.left end,
           func = function() stepMenu(-1) end
         },
         next = {
-          cond = function(e, evs) return e == evs.release.minus end,
+          cond = function(e, evs) return e == evs.dial.right end,
           func = function() stepMenu(1) end
         },
         cnfrm = {
@@ -89,7 +87,7 @@ ctrlSchema = {
             func = function() execMenu() end
         },
         exit = {
-          cond = function(e, evs) return e == evs.release.exit end,
+          cond = function(e, evs) return e == evs.release.exit or e == 1542 end, -- 1542 => [SYS]
           func = function() setState("display") end
         }
     }
